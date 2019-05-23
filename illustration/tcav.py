c960c09f92be2a77a30ad7c0577daaf16fcc3fb4
Kris Sankaran <sankaran.kris@gmail.com>
2019-05-23 00:15:54 -0400

    am not using this jacobian code

--- illustration/tcav.py
+++ illustration/tcav.py
@@ -68,30 +68,3 @@ def concept_scores(jacobians, v):
         S.append(np.dot(jacobians[i].numpy(), v))
 
     return np.vstack(S)
-
-
-from torch.autograd.gradcheck import zero_gradients
-
-def compute_jacobian(inputs, output):
-	"""
-	:param inputs: Batch X Size (e.g. Depth X Width X Height)
-	:param output: Batch X Classes
-	:return: jacobian: Batch X Classes X Size
-	"""
-	num_classes = output.size()[0]
-
-	jacobian = torch.zeros(num_classes, *inputs.size())
-	grad_output = torch.zeros(*output.size())
-	if inputs.is_cuda:
-		grad_output = grad_output.cuda()
-		jacobian = jacobian.cuda()
-
-	for i in range(num_classes):
-		zero_gradients(inputs)
-		grad_output.zero_()
-		grad_output[i] = 1
-		output.backward(grad_output, retain_graph=True)
-		jacobian[i] = inputs.grad.data
-
-	return torch.transpose(jacobian, dim0=0, dim1=1)
-

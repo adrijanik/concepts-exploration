ca5d8ef11d4380018fff723a6de3fb1c2c01da6f
Kris Sankaran <sankaran.kris@gmail.com>
2019-05-26 15:59:49 -0400

    creating some plots about cluster activations

--- illustration/exper.py
+++ illustration/exper.py
@@ -9,11 +9,13 @@ import torch
 import torch.nn.functional as F
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+np.random.seed(100)
+
 # generate some data
-n = 1000
+n = 2000
 x = np.random.uniform(-1, 1, size=(n, 2))
 y = np.zeros(n)
-circle_ix = x[:, 0] ** 2 + x[:, 1] ** 2 <= .5 ** 2
+circle_ix = x[:, 0] ** 2 + x[:, 1] ** 2 <= .25 ** 2
 y[circle_ix] = 1
 y[np.logical_not(circle_ix) * x[:, 1] > 0] = 2
 data = m.DF(x, y.astype(np.int64))
@@ -52,13 +54,13 @@ pd.DataFrame(model.hy.bias.detach().numpy()).to_csv("b2.csv", index=False)
 # directional derivatives, on random directions in H dimensional space
 _, _, v = np.linalg.svd(combined.iloc[:, 6:].values)
 vs = []
-for j in range(2000):
-    coefs = np.random.normal(0, 1, len(v))
+for j in range(500):
+    coefs = np.random.normal(0, 1, H)
     coefs /= np.sqrt(sum(coefs ** 2))
     # vs.append(np.dot(v[:, :3], coefs))
     vs.append(coefs)
 
-concepts_scores(J, vs).to_csv("scores_rand.csv", index=False)
+tcav.concepts_scores(J, vs).to_csv("scores_rand.csv", index=False)
 pd.DataFrame(vs).to_csv("v_rand.csv", index=False)
 
 kmeans = KMeans(n_clusters=50).fit(x)
@@ -74,9 +76,12 @@ for k in range(50):
         cur_data.append(data[i])
 
     J = tcav.all_jacobians(model, cur_data)
-    cur_scores = pd.DataFrame(concept_scores(J, v))
+    cur_scores = pd.DataFrame(tcav.concept_scores(J, v))
     cur_scores["sample"] = np.where(ix)[0]
     cur_scores["k"] = k
     scores.append(cur_scores)
 
 pd.concat(scores).to_csv("scores_cluster.csv", index=False)
+pd.DataFrame(kmeans.cluster_centers_).to_csv("cluster_centroids.csv", index=False)
+
+scores
--- illustration/plot.R
+++ illustration/plot.R
@@ -8,10 +8,16 @@ library("ggplot2")
 library("dplyr")
 library("reshape2")
 library("readr")
+library("locfdr")
 theme_set(theme_bw() + theme(panel.grid = element_blank()))
 sigm <- function(x) {
   exp(-x) / (1 + exp(-x))
 }
+
+negvar <- function(x) {
+  -var(x)
+}
+
 
 #' read in data
 combined <- read_csv("combined.csv") %>%
@@ -32,7 +38,7 @@ for (k in seq_len(K)) {
   probs[[k]] <- data.frame(
     w_k = k,
     u = u,
-    p = sigm(u %*% w1[k, ] + b1[k, ])
+    p = (u %*% w1[k, ] + b1[k, ]) * as.numeric(I((u %*% w1[k, ] + b1[k, ]) > 0))
   )
   colnames(probs[[k]]) <- c("w_k", "X1", "X2", "p")
 }
@@ -52,7 +58,7 @@ concept_scores <- combined %>%
   select(sample, y, starts_with("X"), starts_with("p"), starts_with("h")) %>%
   left_join(scores)
 
-cur_levels <- c("7", "18", "12", "2")
+cur_levels <- c("7", "8", "1")
 
 w1 <- data.frame(w1) %>%
   mutate(index = factor(1:n(), levels = w_order)) %>%
@@ -76,13 +82,8 @@ scores_rand <- read_csv("scores_rand.csv") %>%
 
 scores_rand_v <- scores_rand %>%
   group_by(index, class) %>%
-  summarise(sigma2 = var(score)) %>%
+  summarise(sigma2 = sd(score)) %>%
   arrange(desc(sigma2))
-
-scores_rand_frac <- scores_rand %>%
-  group_by(index, class) %>%
-  summarise(pos_frac = quantile(score, 0.95)) %>%
-  arrange(desc(pos_frac))
 
 #' scores from random positions
 scores_cluster <- read_csv("scores_cluster.csv") %>%
@@ -91,10 +92,6 @@ scores_cluster <- read_csv("scores_cluster.csv") %>%
 scores_cluster_v <- scores_cluster %>%
   group_by(k, class) %>%
   summarise(sigma2 = var(score))
-
-#' fdr result on these scores
-locfdr(scores_rand_v$sigma2, nulltype = 1)
-locfdr(scores_cluster_v$sigma2, nulltype = 1)
 
 #' all the plots
 ggplot(concept_scores %>%
@@ -105,26 +102,26 @@ ggplot(concept_scores %>%
   ) +
   geom_point(
     aes(
-      x = X1, y = X2, col = score
+      x = X1, y = X2, col = y, alpha = abs(score), size = abs(score)
     ),
-    size = 0.3
   ) +
-  scale_color_gradient2() +
+  labs(
+    "size" = "Absolute Concept Activation",
+    "alpha" = "Absolute Concept Activation",
+    "color" = "Class",
+    "fill" = "Learned Feature"
+  ) +
+  scale_y_continuous(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_color_brewer(palette = "Set2") +
+  scale_alpha(range = c(0.2, 1)) +
+  scale_size(range = c(0.1, 2)) +
   scale_fill_gradient2(low = "white", high = "black") +
-  facet_grid(class ~ index)
+  facet_grid(class ~ index) +
+  coord_fixed() +
+  theme(legend.position = "bottom", legend.direction = "vertical")
 
-ggplot(concept_scores) +
-  geom_histogram(
-    aes(
-      x = score,
-      fill = class
-    ),
-    position = "identity",
-    bins = 50,
-    alpha = 0.2
-  ) +
-  ylim(0, 40) +
-  facet_wrap(~ index)
+ggsave("activations_with_surface.png", width = 5, height = 8, dpi = 300)
 
 ggplot(w1) +
   geom_text(
@@ -139,12 +136,20 @@ ggplot(p_hat) +
   geom_point(
     data = combined,
     aes(x = X1, y = X2, col = y),
-    size = 0.6
+    size = 0.3, alpha = 0.6
+  ) +
+  labs(
+    "color" = "Class",
+    "fill" = "Predicted Probability"
   ) +
   scale_color_brewer(palette = "Set2") +
+  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) +
   scale_fill_gradient2(midpoint = 0.5, low = "white", high = "black") +
   coord_fixed() +
-  facet_wrap(~ variable)
+  facet_wrap(~ variable) +
+  theme(legend.position = "bottom")
+
+ggsave("p_hat_surface.png", width = 5, height = 3, dpi=250)
 
 ggplot(scores_rand %>% sample_n(10000)) +
   geom_point(
@@ -156,11 +161,20 @@ ggplot(scores_rand %>% sample_n(10000)) +
 
 ggplot(scores_cluster) +
   geom_point(
-    aes(x = reorder(k, score, var), y = score, col = as.factor(class)),
-    alpha = 0.6
+    aes(x = reorder(k, score, negvar), y = score, col = as.factor(class)),
+    alpha = 0.6, size = 0.9,
+    position = position_jitter(0.2, 0.001)
   ) +
+  labs(
+    "x" = "Cluster",
+    "y" = "Concept Activation",
+    "col" = "Class"
+  ) +
+  facet_wrap(~ class) +
   scale_color_brewer(palette = "Set2") +
-  theme(axis.text.x = element_blank())
+  theme(axis.text.x = element_blank(), legend.position = "bottom", axis.ticks = element_blank())
+
+ggsave("cluster_by_activation.png", dpi = 400)
 
 ggplot(concept_scores %>% filter(class == "class_0")) +
   geom_point(
@@ -189,77 +203,135 @@ ggplot(mw1) +
   scale_fill_gradient2(high = "royalblue", low = "indianred")
 
 ggplot(scores_rand_v) +
-  geom_histogram(aes(x = sqrt(sigma2)), bins = 100)
+  geom_histogram(aes(x = sigma2), bins = 100)
 
-fdr_fit <- locfdr(sqrt(scores_rand_v$sigma2))
+fdr_fit <- locfdr(scores_rand_v$sigma2)
+scores_rand_v$fdr <- fdr_fit$fdr
 
-fdr_fits <- list()
-for (k in unique(scores_rand_v$class)) {
-  fdr_fits[[k]] <- scores_rand_v %>%
-    filter(class == k) %>%
-    .[["sigma2"]] %>%
-    sqrt() %>%
-    locfdr()
-}
+joined <- scores_rand %>%
+  left_join(scores_rand_v)
 
-ggplot(scores_rand_frac) +
-  geom_histogram(aes(x = sqrt(pos_frac)), bins = 100)
+ggplot(joined %>% sample_n(10000)) +
+  geom_point(
+    aes(x = reorder(index, score, negvar), y = score, col = as.factor(class), alpha = fdr, size = fdr),
+    position = position_jitter(0, 0.04)
+  ) +
+  scale_alpha(range = c(1, 0.1)) +
+  scale_size(range = c(4, 0.2)) +
+  scale_color_brewer(palette = "Set2") +
+  theme(axis.text.x = element_blank()) +
+  labs(
+    x = "Random Direction",
+    y = "Concept Activation Score",
+    size = "False Discovery Rate",
+    alpha = "False Discovery Rate",
+    color = "Class"
+  ) +
+  theme(legend.position =  "bottom", axis.ticks = element_blank())
 
-pvals <- read_csv("pvals.csv") %>%
-  melt(variable.name = "class", value.name = "p")
 
-ggplot(pvals) +
-  geom_histogram(aes(x = p), bins = 100) +
-  facet_grid(class ~ .)
+ggsave("fdr_by_activation.png", dpi = 400)
+
 
 vs <- read_csv("v_rand.csv")
-colnames(vs) <- paste0("V", colnames(vs))
 
-vs_sup <- vs %>%
-  mutate(
-    index = 1:n(),
-    fdr0 =	fdr_fits[[1]]$fdr,
-    fdr1 =	fdr_fits[[2]]$fdr,
-    fdr2 =	fdr_fits[[3]]$fdr
-  )
-
-t(as.matrix(w1[, 1:2])) %*% vs[1, ]
-
-direction_scores <- matrix(nrow = nrow(vs), ncol =  5)
+directions <- list()
 for (i in seq_len(nrow(vs))) {
-  wv <- t(w1[, 1:2]) %*% unlist(vs[i, ])
-  direction_scores[i, ] <- c(
-    wv,
-    fdr_fits[[1]]$fdr[i],
-    fdr_fits[[2]]$fdr[i],
-    fdr_fits[[3]]$fdr[i]
-  )
+  directions[[i]] <- t(as.matrix(w1[, 1:2])) %*% as.numeric(vs[i, ])
+  directions[[i]] <- directions[[i]] / sqrt(sum(directions[[i]] ** 2))
 }
-direction_scores <- data.frame(direction_scores)
-colnames(direction_scores) <- c("x", "y", "fdr0", "fdr1", "fdr2")
+directions <- t(do.call(cbind, directions))
+directions <- data.frame(directions)
 
-ggplot(direction_scores) +
-  geom_point(
-    aes(x = x, y = y, alpha = fdr2)
-  ) +
-  scale_alpha(range = c(1, 0.05))
+for (k in 0:2) {
+  directions[[paste0("fdr_", k)]] <- scores_rand_v %>%
+    filter(class == k) %>%
+    arrange(index) %>%
+    .[["fdr"]]
+}
 
-ggplot(direction_scores) +
-  geom_point(aes(x = x, y = fdr2))
-
-
-ggplot(scores_rand %>% sample_n(10000)) +
-  geom_point(
-    aes(x = reorder(index, score, var), y = score, col = as.factor(class)),
-    alpha = 0.2
-  ) +
-  scale_color_brewer(palette = "Set2") +
-  theme(axis.text.x = element_blank())
-
-
-scores_rand$fdr0 <- direction_scores$fdr0[scores_rand$index + 1]
-
-ggplot(scores_rand %>% sample_n(100000)) +
-  geom_point(
-    aes(x = fdr0, y = score, col = class)
+directions <- directions %>%
+  melt(
+    measure.vars = c("fdr_0", "fdr_1", "fdr_2"),
+    value.name =  "fdr",
+    variable.name = "class"
   )
+
+ggplot(directions) +
+  geom_segment(
+    aes(x = 0, y = 0, xend = wj1, yend = wj2, alpha = fdr),
+    size = 0.5
+  ) +
+  geom_point(
+    data = combined,
+    aes(x = X1, y = X2, col = y),
+    size = 0.2
+  ) +
+  geom_point(
+    aes(x = wj1, y = wj2, alpha = fdr, size = fdr)
+  ) +
+  labs(
+    col = "Class",
+    alpha = "Local FDR",
+    size = "Local FDR"
+  ) +
+  guides(color = guide_legend(override.aes = list(size = 2, alpha = 1))) +
+  facet_grid(. ~ class) +
+  scale_color_brewer(palette = "Set2") +
+  scale_alpha(range =  c(1, 0.1)) +
+  scale_size(range =  c(4, 0.2)) +
+  coord_fixed() +
+  theme(legend.position = "bottom")
+
+ggsave("fdr_directions.png", width = 6, height = 4)
+
+
+centroids <- read_csv("cluster_centroids.csv")
+colnames(centroids) <- c("c1", "c2")
+centroids$k <- 0:(nrow(centroids) - 1)
+
+scores_cluster <- scores_cluster %>%
+  left_join(centroids)
+
+scores_cluster_means <- scores_cluster %>%
+  group_by(k, class, c1, c2) %>%
+  summarise(mu_s = mean(score), sigma_s = sd(score))
+
+combined$sample <- 0:(nrow(combined) - 1)
+
+ggplot(scores_cluster_means) +
+  geom_tile(
+    data = probs %>% filter(w_k == 1),
+    aes(x = X1, y = X2, fill = p)
+  ) +
+  geom_point(
+    aes(x = c1, y = c2, size = sigma_s),
+    shape = 15, col = "#9955bb"
+  ) +
+  geom_point(
+    data = combined %>% left_join(scores_cluster),
+    aes(x = X1, y = X2, col = y, alpha = abs(score)),
+    size = 1.2
+  ) +
+  labs(
+    x = "x",
+    y = "y",
+    col = "Class",
+    size = "SD within Cluster",
+    fill = "Learned Feature",
+    alpha = "Absolute Concept Activation"
+  ) +
+  scale_alpha(range = c(0.05, 1), breaks = c(0.1, 0.25, 0.5)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0, 0)) +
+  facet_grid(. ~ class) +
+  scale_size(range = c(.3, 4)) +
+  coord_fixed() +
+  scale_color_brewer(palette = "Set2") +
+  scale_fill_gradient2(low = "white", high = "black") +
+  guides(fill = guide_colorbar(barheight = 2.5)) +
+  theme(
+    legend.position = "bottom", legend.direction = "vertical"
+  )
+
+ggsave("cluster_activation_sds.png", width = 7, height = 4)
